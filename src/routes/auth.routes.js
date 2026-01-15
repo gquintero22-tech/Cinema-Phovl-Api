982178f704f2554d899a6fc5c1feaa55c37a427e
gquintero22-tech <g.quintero22@info.uas.edu.mx>
2026-01-14 18:19:35 -0700

    Agregacion de funcionalidades

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,7 @@
 const router = require("express").Router();
 const controller = require("../controllers/auth.controller");
-const asientoRoutes = require("./routes/asiento.routes");
 
 router.post("/register", controller.register);
 router.post("/login", controller.login);
 
-app.use("/api", asientoRoutes);
 module.exports = router;
