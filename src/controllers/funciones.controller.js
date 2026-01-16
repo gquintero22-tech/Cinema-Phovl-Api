6e2f4abf73028a554f5d50b848b8132344481ff1
gquintero22-tech <g.quintero22@info.uas.edu.mx>
2026-01-15 23:32:18 -0700

    Ajuste

--- src/controllers/funciones.controller.js
+++ src/controllers/funciones.controller.js
@@ -27,18 +27,15 @@ exports.getByMovieSucursalFecha = async (req, res) => {
 
 exports.getById = async (req, res) => {
   try {
-    const { id_funcion } = req.params;
-    const result = await pool.query(`
-      SELECT f.id_funcion, f.id_sala, f.hora_inicio, f.idioma, f.fecha, f.id_pelicula, s.nombre AS sala, s.id_sucursal
-      FROM funcion f
-      JOIN sala s ON f.id_sala = s.id_sala
-      WHERE f.id_funcion = $1
-    `, [id_funcion]);
+    const { id } = req.params;
+    const result = await pool.query(
+      "SELECT * FROM pelicula WHERE id_pelicula = $1",
+      [id]
+    );
 
-    if (result.rows.length === 0) return res.status(404).json({ error: "Función no encontrada" });
-
+    if (result.rows.length === 0) return res.status(404).json({ error: "Película no encontrada" });
     res.json(result.rows[0]);
   } catch (err) {
-    res.status(500).json({ error: "Error al obtener función" });
+    res.status(500).json({ error: "Error al obtener película" });
   }
 };
