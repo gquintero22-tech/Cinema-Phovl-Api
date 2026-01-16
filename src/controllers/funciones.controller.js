6ac91f8bf3ba6fb7aef4c433722f22ba38f98d4c
gquintero22-tech <g.quintero22@info.uas.edu.mx>
2026-01-15 23:35:44 -0700

    Ajuste

--- src/controllers/funciones.controller.js
+++ src/controllers/funciones.controller.js
@@ -25,17 +25,3 @@ exports.getByMovieSucursalFecha = async (req, res) => {
   }
 };
 
-exports.getById = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const result = await pool.query(
-      "SELECT * FROM pelicula WHERE id_pelicula = $1",
-      [id]
-    );
-
-    if (result.rows.length === 0) return res.status(404).json({ error: "Película no encontrada" });
-    res.json(result.rows[0]);
-  } catch (err) {
-    res.status(500).json({ error: "Error al obtener película" });
-  }
-};
