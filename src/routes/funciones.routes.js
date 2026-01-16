5d12c124f8efaa2d173c2db52bf437ae585c7979
gquintero22-tech <g.quintero22@info.uas.edu.mx>
2026-01-15 23:35:30 -0700

    Ajuste

--- src/routes/funciones.routes.js
+++ src/routes/funciones.routes.js
@@ -2,6 +2,5 @@ const router = require("express").Router();
 const controller = require("../controllers/funciones.controller");
 
 router.get("/:id_pelicula/:id_sucursal/:fecha", controller.getByMovieSucursalFecha);
-router.get("/:id", controller.getById);
 
 module.exports = router;
